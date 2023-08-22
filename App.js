f4424a7051eeb30cc63b15b758b7a216c54b5336
vipin yadav <96611380+vipinyadav31@users.noreply.github.com>
2023-08-22 19:50:14 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import WeatherApp from './Component/Weather/WeatherApp';
+
+
+function App() {
+  return (
+    <>
+    <WeatherApp />
+    </>
+  );
+}
+
+export default App;
